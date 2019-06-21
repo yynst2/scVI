ca1e4502c7166d727a87923382e9095dcd1d2858
Pierre Boyeau <pierre.boyeau@gmail.com>
2019-06-20 17:25:16 -0700

    feat new utils

--- scvi/utils/__init__.py
+++ scvi/utils/__init__.py
@@ -1,6 +1,7 @@
-from .utils import make_dir_if_necessary
+from .utils import make_dir_if_necessary, IterativeDict
 
 
 __all__ = [
     'make_dir_if_necessary',
+    'IterativeDict'
 ]
--- scvi/utils/utils.py
+++ scvi/utils/utils.py
@@ -1,6 +1,21 @@
 import os
+import pandas as pd
 
 
 def make_dir_if_necessary(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+
+class IterativeDict:
+    def __init__(self, model_names):
+        self.values = {key: {} for key in model_names}
+
+    def set_values(self, model_name, metric_name, values):
+        if metric_name not in self.values[model_name]:
+            self.values[model_name][metric_name] = [values]
+        else:
+            self.values[model_name][metric_name].append(values)
+
+    def to_df(self):
+        return pd.DataFrame(self.values)
