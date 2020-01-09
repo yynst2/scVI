eb5af4ae24f29a0655b19b0be3e03706959114f0
Pierre Boyeau <pierre.boyeau@gmail.com>
2020-01-09 17:55:04 +0100

    update experiment scripts

--- mnist_experiment.py
+++ mnist_experiment.py
@@ -6,23 +6,24 @@ from tqdm.auto import tqdm
 
 from scvi.dataset import MnistDataset
 from scvi.inference import MnistTrainer
-from scvi.models import PSIS
 from scvi.models import SemiSupervisedVAE
+from arviz.stats import psislw
 
 NUM = 500
 N_EXPERIMENTS = 10
-labelled_proportions = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.])
+labelled_proportions = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.0])
 labelled_proportions = labelled_proportions / labelled_proportions.sum()
 labelled_fraction = 0.05
 
 n_input = 28 * 28
 n_labels = 9
 
-CLASSIFICATION_RATIO = 50.
-N_EVAL_SAMPLES = 15
-# N_EPOCHS = 100
+CLASSIFICATION_RATIO = 50.0
+N_EVAL_SAMPLES = 20
 N_EPOCHS = 100
-LR = 1e-4
+# N_EPOCHS = 300
+LR = 3e-4
+BATCH_SIZE = 512
 
 dataset = MnistDataset(
     labelled_fraction=labelled_fraction,
@@ -30,20 +31,21 @@ dataset = MnistDataset(
     root="/home/pierre/scVI/tests/mnist",
     download=True,
     do_1d=True,
-    test_size=0.
-    # test_size=0.6
+    test_size=0.0,
+    # test_size=0.0,
 )
 
 print("train all examples", len(dataset.train_dataset.tensors[0]))
 print("train labelled examples", len(dataset.train_dataset_labelled.tensors[0]))
 
 scenarios = [  # WAKE updates
-    ("ELBO", "ELBO", "ELBO", None, ),
-    (None, "ELBO", "CUBO", None, ),
-    # (None, "ELBO", "REVKL", None,),
+    # ( overall_training, loss_gen, loss_wvar, loss_svar, n_samples_train, n_samples_wtheta, n_samples_wphi,)
+    (None, "ELBO", "REVKL", None, None, 1, 15, False),
+    ("ELBO", "ELBO", "ELBO", None, 1, None, None, True),
+    # (None, "ELBO", "CUBO", None, None, 1, 15, True),
 ]
 
-df = []
+df_li = []
 
 
 # Utils functions
@@ -51,7 +53,7 @@ def compute_reject_score(y_true: np.ndarray, y_pred: np.ndarray, num=20):
     n_examples, n_pos_classes = y_pred.shape
 
     assert np.unique(y_true).max() == (n_pos_classes - 1) + 1
-    thetas = np.linspace(0.1, 1., num=num)
+    thetas = np.linspace(0.1, 1.0, num=num)
     res = dict(
         precision_discovery=np.zeros(num),
         recall_discovery=np.zeros(num),
@@ -68,14 +70,25 @@ def compute_reject_score(y_true: np.ndarray, y_pred: np.ndarray, num=20):
 
         y_true_discovery = y_true == (n_pos_classes - 1) + 1
         y_pred_discovery = y_pred_theta == (n_pos_classes - 1) + 1
-        res["precision_discovery"][idx] = precision_score(y_true_discovery, y_pred_discovery)
+        res["precision_discovery"][idx] = precision_score(
+            y_true_discovery, y_pred_discovery
+        )
         res["recall_discovery"][idx] = recall_score(y_true_discovery, y_pred_discovery)
     return res
 
 
 # Main script
 for scenario in tqdm(scenarios):
-    overall_training, loss_gen, loss_wvar, loss_svar = scenario
+    (
+        overall_training,
+        loss_gen,
+        loss_wvar,
+        loss_svar,
+        n_samples_train,
+        n_samples_wtheta,
+        n_samples_wphi,
+        reparam_latent
+    ) = scenario
     iwelbo = []
     cubo = []
     khat = []
@@ -83,6 +96,7 @@ for scenario in tqdm(scenarios):
     m_ap_arr = []
     m_recall_arr = []
     auc_pr_arr = []
+    entropy_arr = []
 
     for t in tqdm(range(N_EXPERIMENTS)):
         mdl = SemiSupervisedVAE(
@@ -94,63 +108,87 @@ for scenario in tqdm(scenarios):
             do_batch_norm=True,
         )
         mdl = mdl.cuda()
-        trainer = MnistTrainer(dataset=dataset, model=mdl, use_cuda=True, batch_size=256)
-        trainer.train(
-            n_epochs=N_EPOCHS,
-            lr=LR,
-            overall_loss=overall_training,
-            wake_theta=loss_gen,
-            wake_psi=loss_wvar,
-            classification_ratio=CLASSIFICATION_RATIO,
-            update_mode="all"
+        trainer = MnistTrainer(
+            dataset=dataset, model=mdl, use_cuda=True, batch_size=BATCH_SIZE
         )
 
-        # Eval
-        with torch.no_grad():
-            train_res = trainer.inference(
-                trainer.train_loader,
-                keys=[
-                    "qc_z1_all_probas",
-                    "y",
-                    "CUBO",
-                    "IWELBO",
-                    "log_ratios",
-                ],
-                n_samples=N_EVAL_SAMPLES,
+        try:
+            trainer.train(
+                n_epochs=N_EPOCHS,
+                lr=LR,
+                overall_loss=overall_training,
+                wake_theta=loss_gen,
+                wake_psi=loss_wvar,
+                n_samples=n_samples_train,
+                n_samples_theta=n_samples_wtheta,
+                n_samples_phi=n_samples_wphi,
+                classification_ratio=CLASSIFICATION_RATIO,
+                update_mode="all",
             )
-        y_pred = train_res["qc_z1_all_probas"].mean(0).numpy()
-        y_true = train_res["y"].numpy()
 
-        # Choice right now: all log-ratios related metrics are computed in the unsupervised case
+            # Eval
+            with torch.no_grad():
+                train_res = trainer.inference(
+                    trainer.train_loader,
+                    keys=["qc_z1_all_probas", "y", "CUBO", "IWELBO", "log_ratios"],
+                    n_samples=N_EVAL_SAMPLES,
+                )
+            y_pred = train_res["qc_z1_all_probas"].mean(0).numpy()
+            y_true = train_res["y"].numpy()
 
-        # Precision / Recall for discovery class
-        # And accuracy
-        res_baseline = compute_reject_score(y_true=y_true, y_pred=y_pred, num=NUM)
-        m_accuracy = res_baseline["accuracy"]
-        m_ap = res_baseline["precision_discovery"]
-        m_recall = res_baseline["recall_discovery"]
-        auc_pr = np.trapz(
-            x=res_baseline["recall_discovery"],
-            y=res_baseline["precision_discovery"],
-        )
+            # Choice right now: all log-ratios related metrics are computed in the unsupervised case
 
-        m_accuracy_arr.append(m_accuracy)
-        m_ap_arr.append(m_ap)
-        m_recall_arr.append(m_recall)
-        auc_pr_arr.append(auc_pr)
+            # Precision / Recall for discovery class
+            # And accuracy
+            res_baseline = compute_reject_score(y_true=y_true, y_pred=y_pred, num=NUM)
+            # m_accuracy = res_baseline["accuracy"]
+            m_ap = res_baseline["precision_discovery"]
+            m_recall = res_baseline["recall_discovery"]
+            auc_pr = np.trapz(
+                x=res_baseline["recall_discovery"],
+                y=res_baseline["precision_discovery"],
+            )
 
-        # Cubo / Iwelbo
-        cubo_sam = train_res["CUBO"]
-        cubo.append(cubo_sam.mean())
+            # m_accuracy_arr.append(m_accuracy)
+            m_ap_arr.append(m_ap)
+            m_recall_arr.append(m_recall)
+            auc_pr_arr.append(auc_pr)
 
-        # k_hat
-        log_ratios = train_res["log_ratios"]
-        ratios = log_ratios.exp().cpu().numpy()
-        psis = PSIS(num_samples=N_EVAL_SAMPLES)
-        psis.fit(ratios)
+            # Cubo / Iwelbo
+            cubo_sam = train_res["CUBO"]
+            cubo.append(cubo_sam.mean())
 
-        shapes = psis.shape
-        khat.append(np.array(shapes).mean())
+            iwelbo_sam = train_res["IWELBO"]
+            iwelbo.append(iwelbo_sam.mean())
+
+            # Entropy
+            where9 = train_res["y"] == 9
+            probas9 = train_res["qc_z1_all_probas"].mean(0)[where9]
+            entropy_arr.append((-probas9 * probas9.log()).sum(-1).mean(0))
+
+            where_non9 = train_res["y"] != 9
+            y_non9 = train_res["y"][where_non9]
+            y_pred_non9 = train_res["qc_z1_all_probas"].mean(0)[where_non9].argmax(1)
+            m_accuracy = accuracy_score(y_non9, y_pred_non9)
+            m_accuracy_arr.append(m_accuracy)
+
+            # k_hat
+            log_ratios = train_res["log_ratios"].cpu().numpy()
+            # ratios = log_ratios.exp().cpu().numpy()
+            # psis = PSIS(num_samples=N_EVAL_SAMPLES)
+            # psis.fit(ratios)
+            # shapes = psis.shape
+            # khat.append(np.array(shapes).mean())
+            
+            n_examples = log_ratios.shape[-1]
+            log_ratios = log_ratios.reshape((-1, n_examples)).T
+            assert log_ratios.shapes[0] == n_examples
+            _, khat_vals = psislw(log_ratios)
+            khat.append(khat_vals)
+
+        except Exception as e:
+            print(e)
+            pass
 
     res = {
         "CONFIGURATION": scenario,
@@ -167,8 +205,13 @@ for scenario in tqdm(scenarios):
         "MEAN_AP": np.array(m_ap_arr),
         "MEAN_RECALL": np.array(m_recall_arr),
         "AUC": np.array(auc_pr_arr),
+        "ENTROPY": np.array(entropy_arr),
     }
-    df.append(res)
+    df_li.append(res)
+    df = pd.DataFrame(df_li)
+    df.to_csv("simu_mnist_res_paper.csv", sep="\t")
+    df.to_pickle("simu_mnist_res_paper.pkl")
 
-df = pd.DataFrame(df)
-df.to_csv("simu_gaussian_res_paper.csv", sep="\t")
+df = pd.DataFrame(df_li)
+df.to_csv("simu_mnist_res_paper.csv", sep="\t")
+df.to_pickle("simu_mnist_res_paper.pkl")
--- simu_gaussian.py
+++ simu_gaussian.py
@@ -9,11 +9,15 @@ from scipy.linalg import sqrtm
 from scipy.stats import norm
 import torch
 from tqdm.auto import tqdm
+from arviz.stats import psislw
 
 import sys
 
+FILENAME = "simu_gaussian_res_paper_variable_complexity"
 sys.stdout = open("figures/log_gaussian_training.txt", "w")
 print("STARTED TRAINING", flush=True)
+n_simu = 5
+
 # dim_z = 10
 # dim_x = 100
 # n_epochs = 50
@@ -37,7 +41,7 @@ LINEAR_ENCODER = False
 # learn_var, wake only, sleep only, wake-sleep, linear_encoder for each loss
 scenarios = [  # WAKE updates
     (False, None, "ELBO", None, LINEAR_ENCODER),
-#     (False, None, "IWELBO", None),
+    #     (False, None, "IWELBO", None),
     (False, None, "REVKL", None, LINEAR_ENCODER),
     (False, None, "CUBO", None, LINEAR_ENCODER),
     # (False, None, "VRMAX", None),
@@ -52,10 +56,10 @@ scenarios = [  # WAKE updates
     # (True, "ELBO", None, "SLEEPKL"),
     # (True, "ELBO", "REVKL", "SLEEPKL"),
     # IWELBO and WAKE updates
-#     (True, "IWELBO", "ELBO", None),
+    #     (True, "IWELBO", "ELBO", None),
     # (True, "IWELBO", "IWELBO", None),
-#     (True, "IWELBO", "REVKL", None),
-#     (True, "IWELBO", "CUBO", None),
+    #     (True, "IWELBO", "REVKL", None),
+    #     (True, "IWELBO", "CUBO", None),
     # (True, "IWELBO", "VRMAX", None),
     # IWELBO and SLEEP updates
     # (True, "IWELBO", None, "SLEEPKL"),
@@ -65,151 +69,185 @@ scenarios = [  # WAKE updates
 ]
 
 nus = np.geomspace(1e-4, 1e2, num=20)
+n_hidden_ranges = [16, 32, 64, 128, 256, 512]
+# n_hidden_ranges = [128]
 
 df = []
-for learn_var, loss_gen, loss_wvar, loss_svar, do_linear_encoder in (scenarios):
-    print(learn_var, loss_gen, loss_wvar, loss_svar)
-    iwelbo = []
-    cubo = []
-    l1_gen_dis = []
-    l1_gen_sign = []
-    l1_post_dis = []
-    l1_post_sign = []
-    l1_err_ex_plugin = []
-    l1_err_ex_is = []
-    l2_ess = []
-    l1_errs_is = []
-    khat = []
-    a_2 = []
+for learn_var, loss_gen, loss_wvar, loss_svar, do_linear_encoder in scenarios:
+    for n_hidden in tqdm(n_hidden_ranges):
+        print(learn_var, loss_gen, loss_wvar, loss_svar)
+        iwelbo = []
+        cubo = []
+        l1_gen_dis = []
+        l1_gen_sign = []
+        l1_post_dis = []
+        l1_post_sign = []
+        l1_err_ex_plugin = []
+        l1_err_ex_is = []
+        l2_ess = []
+        l1_errs_is = []
+        khat = []
+        a_2 = []
+        for t in tqdm(range(n_simu)):
+            print(t)
+            params_gen = None
+            params_svar = None
+            params_wvar = None
+            learn_var = False
 
-    for t in tqdm(range(10)):
-        print(t)
-        params_gen = None
-        params_svar = None
-        params_wvar = None
-        learn_var = False
+            if loss_gen is not None:
+                learn_var = True
 
-        if loss_gen is not None:
-            learn_var = True
+            model = LinearGaussian(
+                dataset.A,
+                dataset.pxz_log_det,
+                dataset.pxz_inv_sqrt,
+                gamma=dataset.gamma,
+                n_latent=dim_z,
+                n_input=dim_x,
+                learn_var=learn_var,
+                linear_encoder=do_linear_encoder,
+                n_hidden=n_hidden,
+            )
 
-        model = LinearGaussian(
-            dataset.A,
-            dataset.pxz_log_det,
-            dataset.pxz_inv_sqrt,
-            gamma=dataset.gamma,
-            n_latent=dim_z,
-            n_input=dim_x,
-            learn_var=learn_var,
-            linear_encoder=do_linear_encoder,
-        )
+            trainer = GaussianTrainer(
+                model, dataset, train_size=0.8, use_cuda=True, frequency=5
+            )
 
-        trainer = GaussianTrainer(
-            model, dataset, train_size=0.8, use_cuda=True, frequency=5
-        )
+            if loss_gen is not None:
+                params_gen = [model.px_log_diag_var]
+            if loss_wvar is not None:
+                params_wvar = filter(
+                    lambda p: p.requires_grad, model.encoder.parameters()
+                )
+            if loss_svar is not None:
+                params_svar = filter(
+                    lambda p: p.requires_grad, model.encoder.parameters()
+                )
 
-        if loss_gen is not None:
-            params_gen = [model.px_log_diag_var]
-        if loss_wvar is not None:
-            params_wvar = filter(lambda p: p.requires_grad, model.encoder.parameters())
-        if loss_svar is not None:
-            params_svar = filter(lambda p: p.requires_grad, model.encoder.parameters())
+            losses = loss_gen, loss_wvar, loss_svar
+            params = params_gen, params_wvar, params_svar
+            trainer.train(params, losses, n_epochs=n_epochs)
 
-        losses = loss_gen, loss_wvar, loss_svar
-        params = params_gen, params_wvar, params_svar
-        trainer.train(params, losses, n_epochs=n_epochs)
+            ll_train_set = trainer.history["elbo_train_set"][1:]
+            ll_test_set = trainer.history["elbo_test_set"][1:]
+            x = np.linspace(0, n_epochs, len(ll_train_set))
+            plt.plot(x, ll_train_set)
+            plt.plot(x, ll_test_set)
+            plt.savefig("figures/training_stats.png")
+            plt.clf()
 
-        ll_train_set = trainer.history["elbo_train_set"][1:]
-        ll_test_set = trainer.history["elbo_test_set"][1:]
-        x = np.linspace(0, n_epochs, len(ll_train_set))
-        plt.plot(x, ll_train_set)
-        plt.plot(x, ll_test_set)
-        plt.savefig("figures/training_stats.png")
-        plt.clf()
+            trainer.test_set.elbo()
+            iwelbo += [trainer.test_set.iwelbo(100)]
+            trainer.test_set.exact_log_likelihood()
+            cubo += [trainer.test_set.cubo(100)]
 
-        trainer.test_set.elbo()
-        iwelbo += [trainer.test_set.iwelbo(100)]
-        trainer.test_set.exact_log_likelihood()
-        cubo += [trainer.test_set.cubo(100)]
+            gen_loss_var = (
+                np.diag(dataset.gamma) - model.get_std().cpu().detach().numpy() ** 2
+            )
+            if do_linear_encoder:
+                post_loss_var = np.diag(
+                    dataset.pz_condx_var
+                    - model.encoder.var_encoder.detach().cpu().numpy()
+                )
+            else:
+                post_loss_var = (
+                    np.diag(dataset.pz_condx_var) - trainer.test_set.posterior_var()
+                )
 
-        gen_loss_var = (
-            np.diag(dataset.gamma) - model.get_std().cpu().detach().numpy() ** 2
-        )
-        if do_linear_encoder:
-            post_loss_var = np.diag(dataset.pz_condx_var - model.encoder.var_encoder.detach().cpu().numpy())
-        else:
-            post_loss_var = np.diag(dataset.pz_condx_var) - trainer.test_set.posterior_var()
+            l1_gen_dis += [np.mean(np.abs(gen_loss_var))]
+            l1_gen_sign += [np.mean(np.sign(gen_loss_var))]
+            # generator loss variance
+            # posterior loss variance
 
-        l1_gen_dis += [np.mean(np.abs(gen_loss_var))]
-        l1_gen_sign += [np.mean(np.sign(gen_loss_var))]
-        # generator loss variance
-        # posterior loss variance
+            l1_post_dis += [np.mean(np.abs(post_loss_var))]
+            l1_post_sign += [np.mean(np.sign(post_loss_var))]
 
-        l1_post_dis += [np.mean(np.abs(post_loss_var))]
-        l1_post_sign += [np.mean(np.sign(post_loss_var))]
+            # posterior query evaluation: groundtruth
+            seq = trainer.test_set.sequential(batch_size=10)
+            mean = np.dot(dataset.mz_cond_x_mean, dataset.X[seq.indices, :].T)[0, :]
+            std = np.sqrt(dataset.pz_condx_var[0, 0])
+            exact_cdf = norm.cdf(0, loc=mean, scale=std)
+            # posterior query evaluation: aproposal distribution
+            seq_mean, seq_var, is_cdf, ess = seq.prob_eval(1000)
+            if not do_linear_encoder:
+                plugin_cdf = norm.cdf(
+                    0, loc=seq_mean[:, 0], scale=np.sqrt(seq_var[:, 0])
+                )
+            else:
+                plugin_cdf = norm.cdf(
+                    0, loc=seq_mean[:, 0], scale=np.sqrt(seq_var[0, 0])
+                )  # In this case the encoder learns a constant full
 
-        # posterior query evaluation: groundtruth
-        seq = trainer.test_set.sequential(batch_size=10)
-        mean = np.dot(dataset.mz_cond_x_mean, dataset.X[seq.indices, :].T)[0, :]
-        std = np.sqrt(dataset.pz_condx_var[0, 0])
-        exact_cdf = norm.cdf(0, loc=mean, scale=std)
-        # posterior query evaluation: aproposal distribution
-        seq_mean, seq_var, is_cdf, ess = seq.prob_eval(1000)
-        if not do_linear_encoder:
-            plugin_cdf = norm.cdf(0, loc=seq_mean[:, 0], scale=np.sqrt(seq_var[:, 0]))
-        else:
-            plugin_cdf = norm.cdf(0, loc=seq_mean[:, 0], scale=np.sqrt(seq_var[0, 0]))  # In this case the encoder learns a constant full
+            l1_err_ex_plugin += [np.mean(np.abs(exact_cdf - plugin_cdf))]
+            l1_err_ex_is += [np.mean(np.abs(exact_cdf - is_cdf))]
+            l2_ess += [ess]
 
-        l1_err_ex_plugin += [np.mean(np.abs(exact_cdf - plugin_cdf))]
-        l1_err_ex_is += [np.mean(np.abs(exact_cdf - is_cdf))]
-        l2_ess += [ess]
+            # IS L1_err comparison
+            is_cdf_nus = seq.prob_eval(1000, nu=nus)[2]
+            exact_cdfs_nus = np.array(
+                [norm.cdf(nu, loc=mean, scale=std) for nu in nus]
+            ).T
+            l1_errs_is += [np.abs(is_cdf_nus - exact_cdfs_nus).mean(0)]
 
-        # IS L1_err comparison
-        is_cdf_nus = seq.prob_eval(1000, nu=nus)[2]
-        exact_cdfs_nus = np.array([norm.cdf(nu, loc=mean, scale=std) for nu in nus]).T
-        l1_errs_is += [np.abs(is_cdf_nus - exact_cdfs_nus).mean(0)]
+            # k_hat
+            # ratios = (
+            #     trainer.test_set.log_ratios(n_samples_mc=100).exp().detach().numpy()
+            # )
+            # psis = PSIS(num_samples=100)
+            # psis.fit(ratios)
+            log_ratios = trainer.test_set.log_ratios(n_samples_mc=100).detach().numpy()
+            # Input should be n_obs, n_samples
+            log_ratios = log_ratios.T
+            assert log_ratios.shape[1] == 100
+            _, khat_vals = psislw(log_ratios)
+            khat.append(khat_vals)
 
-        # k_hat
-        ratios = trainer.test_set.log_ratios(n_samples_mc=100).exp().detach().numpy()
-        psis = PSIS(num_samples=100)
-        psis.fit(ratios)
-
-        shapes = psis.shape
-        khat.append(np.array(shapes).mean())
-
-        # a norm
-        # model.eval()
-        gt_post_var = dataset.pz_condx_var
-        sigma_sqrt = sqrtm(gt_post_var)
-        if do_linear_encoder:
-            var_post_var = model.encoder.var_encoder.detach().cpu().numpy()
-            d_inv = np.linalg.inv(var_post_var)
-            a = sigma_sqrt @ (d_inv @ sigma_sqrt) - np.eye(dim_z)
-            a_2_it = np.linalg.norm(a, ord=2)
-        else:
-            a_2_it = np.zeros(len(seq_var))
-            for it in range(len(seq_var)):
-                seq_var_item = seq_var[it] # Posterior variance
-                d_inv = np.diag(1.0 / seq_var_item)  # Variationnal posterior precision
+            # a norm
+            # model.eval()
+            gt_post_var = dataset.pz_condx_var
+            sigma_sqrt = sqrtm(gt_post_var)
+            if do_linear_encoder:
+                var_post_var = model.encoder.var_encoder.detach().cpu().numpy()
+                d_inv = np.linalg.inv(var_post_var)
                 a = sigma_sqrt @ (d_inv @ sigma_sqrt) - np.eye(dim_z)
-                a_2_it[it] = np.linalg.norm(a, ord=2)
-            a_2_it = a_2_it.mean()
-        a_2.append(a_2_it)
+                a_2_it = np.linalg.norm(a, ord=2)
+            else:
+                a_2_it = np.zeros(len(seq_var))
+                for it in range(len(seq_var)):
+                    seq_var_item = seq_var[it]  # Posterior variance
+                    d_inv = np.diag(
+                        1.0 / seq_var_item
+                    )  # Variationnal posterior precision
+                    a = sigma_sqrt @ (d_inv @ sigma_sqrt) - np.eye(dim_z)
+                    a_2_it[it] = np.linalg.norm(a, ord=2)
+                a_2_it = a_2_it.mean()
+            a_2.append(a_2_it)
 
-    res = {
-        "CONFIGURATION": (learn_var, loss_gen, loss_wvar, loss_svar),
-        "IWELBO": (np.mean(iwelbo), np.std(iwelbo)),
-        "CUBO": (np.mean(iwelbo), np.std(iwelbo)),
-        "L1 loss gen_variance_dis": (np.mean(l1_gen_dis), np.std(l1_gen_dis)),
-        "L1 loss gen_variance_sign": (np.mean(l1_gen_sign), np.std(l1_gen_sign)),
-        "L1 loss post_variance_dis": (np.mean(l1_post_dis), np.std(l1_post_dis)),
-        "L1 loss post_variance_sign": (np.mean(l1_post_sign), np.std(l1_post_sign)),
-        "AVE L1 ERROR EXACT <-> PLUGIN": (np.mean(l1_err_ex_plugin), np.std(l1_err_ex_plugin)),
-        "ESS": (np.mean(l2_ess), np.std(l2_ess)),
-        "L1_IS_ERRS": np.array(l1_errs_is),
-        "KHAT": np.array(khat),
-        "A_2": np.array(a_2),
-    }
-    df.append(res)
+        res = {
+            "CONFIGURATION": (learn_var, loss_gen, loss_wvar, loss_svar),
+            "learn_var": learn_var, 
+            "loss_gen": loss_gen, 
+            "loss_wvar": loss_wvar, 
+            "loss_svar": loss_svar,
+            "n_hidden": n_hidden,
+            "IWELBO": (np.mean(iwelbo), np.std(iwelbo)),
+            "CUBO": (np.mean(iwelbo), np.std(iwelbo)),
+            "L1 loss gen_variance_dis": (np.mean(l1_gen_dis), np.std(l1_gen_dis)),
+            "L1 loss gen_variance_sign": (np.mean(l1_gen_sign), np.std(l1_gen_sign)),
+            "L1 loss post_variance_dis": (np.mean(l1_post_dis), np.std(l1_post_dis)),
+            "L1 loss post_variance_sign": (np.mean(l1_post_sign), np.std(l1_post_sign)),
+            "AVE L1 ERROR EXACT <-> PLUGIN": (
+                np.mean(l1_err_ex_plugin),
+                np.std(l1_err_ex_plugin),
+            ),
+            "ESS": (np.mean(l2_ess), np.std(l2_ess)),
+            "L1_IS_ERRS": np.array(l1_errs_is),
+            "KHAT": np.array(khat),
+            "A_2": np.array(a_2),
+        }
+        df.append(res)
 
 df = pd.DataFrame(df)
-df.to_csv("simu_gaussian_res_paper.csv", sep="\t")
+df.to_csv("{}.csv".format(FILENAME), sep="\t")
+df.to_pickle("{}.pkl".format(FILENAME))
