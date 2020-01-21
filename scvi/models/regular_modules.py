850b082396c0dff28b45edc6150d6bb8f5598e2c
Pierre Boyeau <pierre.boyeau@gmail.com>
2020-01-21 11:30:35 +0100

    optim mnist

--- scvi/models/regular_modules.py
+++ scvi/models/regular_modules.py
@@ -11,15 +11,16 @@ logger = logging.getLogger(__name__)
 
 class FCLayersA(nn.Module):
     def __init__(
-        self, n_input, n_output, dropout_rate=0.1, do_batch_norm=True,
+        self, n_input, n_output, n_middle=None, dropout_rate=0.1, do_batch_norm=True,
     ):
         super().__init__()
-        self.to_hidden = nn.Linear(in_features=n_input, out_features=500)
+        n_middle = n_output if n_middle is None else n_middle
+        self.to_hidden = nn.Linear(in_features=n_input, out_features=n_middle)
         self.do_batch_norm = do_batch_norm
         if do_batch_norm:
-            self.batch_norm = nn.BatchNorm1d(num_features=500)
+            self.batch_norm = nn.BatchNorm1d(num_features=n_middle)
 
-        self.to_out = nn.Linear(in_features=500, out_features=n_output)
+        self.to_out = nn.Linear(in_features=n_middle, out_features=n_output)
         self.dropout = nn.Dropout(p=dropout_rate)
         self.activation = nn.SELU()
 
@@ -46,7 +47,7 @@ class FCLayersA(nn.Module):
 
 class EncoderA(nn.Module):
     def __init__(
-        self, n_input, n_output, n_hidden, dropout_rate, do_batch_norm,
+        self, n_input, n_output, n_hidden, dropout_rate, do_batch_norm, n_middle=None,
     ):
         super().__init__()
         self.encoder = FCLayersA(
@@ -54,6 +55,7 @@ class EncoderA(nn.Module):
             n_output=n_hidden,
             dropout_rate=dropout_rate,
             do_batch_norm=do_batch_norm,
+            n_middle=n_middle,
         )
         self.mean_encoder = nn.Linear(n_hidden, n_output)
         self.var_encoder = nn.Linear(n_hidden, n_output)
@@ -225,3 +227,189 @@ class PSIS:
                 self.shape.append(res[0])
             else:
                 raise ValueError("Unknown data results")
+
+
+class EncoderH(nn.Module):
+    def __init__(
+        self,
+        n_in,
+        n_out,
+        # n_cat_list,
+        # n_layers,
+        n_hidden,
+        do_h,
+        dropout_rate,
+        use_batch_norm,
+        do_sigmoid=True,
+    ):
+        """
+        :param n_in:
+        :param n_out:
+        :param n_cat_list:
+        :param n_layers:
+        :param n_hidden:
+        :param do_h:
+        :param dropout_rate:
+        :param use_batch_norm:
+        """
+        super().__init__()
+        self.do_h = do_h
+        self.do_sigmoid = do_sigmoid
+        self.encoder = FCLayersA(
+            n_in=n_in,
+            n_out=n_hidden,
+            dropout_rate=dropout_rate,
+            use_batch_norm=use_batch_norm,
+        )
+        # with torch.no_grad():
+        #     encoder0 =
+        self.mu = nn.Linear(n_hidden, n_out)
+        self.sigma = nn.Linear(n_hidden, n_out)
+        if do_sigmoid:
+            self.init_weights(self.sigma, bias_val=1.5)
+        else:
+            self.init_weights(self.sigma, bias_val=0.5)
+        if do_h:
+            self.h = nn.Linear(n_hidden, n_out)
+
+        self.activation = nn.Sigmoid()
+
+    def forward(self, x, *cat_list: int):
+        """
+        :param x:
+        :param cat_list:
+        :return:
+        """
+        z = self.encoder(x, *cat_list)
+        mu = self.mu(z)
+        sigma = self.sigma(z)
+        sigma = torch.clamp(sigma, min=-50.0)
+        if self.do_sigmoid:
+            sigma = self.activation(sigma)
+        else:
+            # sigma = nn.ReLU()(sigma)
+            sigma = sigma.exp()
+        if (sigma.min() == 0).item():
+            print("tada")
+        if self.do_h:
+            h = self.h(z)
+            return mu, sigma, h
+        return mu, sigma
+
+    @staticmethod
+    def init_weights(m, bias_val=1.5):
+        torch.nn.init.normal_(m.weight, mean=0.0, std=1e-8)
+        torch.nn.init.constant_(m.bias, val=bias_val)
+
+
+class EncoderIAF(nn.Module):
+    def __init__(
+        self,
+        n_in,
+        n_latent,
+        n_cat_list,
+        n_hidden,
+        n_layers,
+        t,
+        dropout_rate=0.05,
+        use_batch_norm=True,
+        do_h=True,
+    ):
+        """
+        Encoder using h representation as described in IAF paper
+        :param n_in:
+        :param n_latent:
+        :param n_cat_list:
+        :param n_hidden:
+        :param n_layers:
+        :param t:
+        :param dropout_rate:
+        :param use_batch_norm:
+        """
+        super().__init__()
+        self.do_h = do_h
+        msg = "" if do_h else "Not "
+        logger.info(msg="{}Using Hidden State".format(msg))
+        self.n_latent = n_latent
+        self.encoders = torch.nn.ModuleList()
+        self.encoders.append(
+            EncoderH(
+                n_in=n_in,
+                n_out=n_latent,
+                n_cat_list=n_cat_list,
+                n_layers=n_layers,
+                n_hidden=n_hidden,
+                do_h=do_h,
+                dropout_rate=dropout_rate,
+                use_batch_norm=use_batch_norm,
+                do_sigmoid=False,
+            )
+        )
+
+        n_in = 2 * n_latent if do_h else n_latent
+        for _ in range(t - 1):
+            self.encoders.append(
+                EncoderH(
+                    n_in=n_in,
+                    n_out=n_latent,
+                    n_cat_list=None,
+                    n_layers=n_layers,
+                    n_hidden=n_hidden,
+                    do_h=False,
+                    dropout_rate=dropout_rate,
+                    use_batch_norm=use_batch_norm,
+                    do_sigmoid=True,
+                )
+            )
+
+        self.dist0 = Normal(
+            loc=torch.zeros(n_latent, device="cuda"),
+            scale=torch.ones(n_latent, device="cuda"),
+        )
+
+    def forward(self, x, *cat_list: int, n_samples=1, reparam=True):
+        """
+        :param x:
+        :param cat_list:
+        :return:
+        """
+        if self.do_h:
+            mu, sigma, h = self.encoders[0](x, *cat_list)
+        else:
+            mu, sigma = self.encoders[0](x, *cat_list)
+            h = None
+
+        # Big issue when x is 3d !!!
+        # Should stay 2d!!
+        sampler = self.dist0.rsample if reparam else self.dist0.sample
+        if n_samples == 1:
+            eps = sampler((len(x),))
+            assert eps.shape == (len(x), self.n_latent)
+        else:
+            eps = sampler((n_samples, len(x),))
+            assert eps.shape == (n_samples, len(x), self.n_latent)
+
+        z = mu + eps * sigma
+        qz_x = sigma.log() + 0.5 * (eps ** 2) + 0.5 * np.log(2.0 * np.pi)
+        qz_x = -qz_x.sum(dim=-1)
+
+        # z shape (n_samples, n_batch, n_latent)
+        if (z.dim() == 3) & (h.dim() == 2):
+            n_batches, n_hdim = h.shape
+            h_it = h.reshape(1, n_batches, n_hdim).expand(n_samples, n_batches, n_hdim)
+        else:
+            h_it = h
+
+        for ar_nn in self.encoders[1:]:
+            if self.do_h:
+                inp = torch.cat([z, h_it], dim=-1)
+            else:
+                inp = z
+            mu_it, sigma_it = ar_nn(inp)
+            z = sigma_it * z + (1.0 - sigma_it) * mu_it
+            new_term = sigma_it.log()
+            qz_x -= new_term.sum(dim=-1)
+
+        if torch.isnan(qz_x).any() or torch.isinf(qz_x).any():
+            print("ouille")
+        return dict(latent=z, posterior_density=qz_x, last_inp=inp)
