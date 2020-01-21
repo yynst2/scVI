1a3cea8b6198fe7f4ec23b958729522ff956589c
Pierre Boyeau <pierre.boyeau@gmail.com>
2020-01-21 18:50:34 +0100

    optim mnist

--- mnist_experiment.py
+++ mnist_experiment.py
@@ -22,9 +22,14 @@ n_labels = 9
 CLASSIFICATION_RATIO = 50.0
 N_EVAL_SAMPLES = 25
 N_EPOCHS = 100
-# N_EPOCHS = 300
 LR = 3e-4
-BATCH_SIZE = 512
+BATCH_SIZE = 1024
+
+FILENAME = "simu_mnist2.pkl"
+MDL_DIR = "models/mnist3"
+
+if not os.path.exists(MDL_DIR):
+    os.makedirs(MDL_DIR)
 
 dataset = MnistDataset(
     labelled_fraction=labelled_fraction,
@@ -35,12 +40,30 @@ dataset = MnistDataset(
     test_size=0.0,
     # test_size=0.0,
 )
+x_train, y_train = dataset.train_dataset.tensors
+rdm_indices = np.random.choice(len(x_train), 64)
+x_sample = x_train[rdm_indices].to("cuda")
+y_sample = y_train[rdm_indices].to("cuda")
+
+where = y_train == 8
+x_sample_supervised = x_train[where][:64]
 
 print("train all examples", len(dataset.train_dataset.tensors[0]))
 print("train labelled examples", len(dataset.train_dataset_labelled.tensors[0]))
 
 scenarios = [  # WAKE updates
     # ( overall_training, loss_gen, loss_wvar, loss_svar, n_samples_train, n_samples_wtheta, n_samples_wphi,)
+    # dict(
+    #     loss_gen="ELBO",
+    #     loss_wvar="CUBOB",
+    #     n_samples_train=None,
+    #     n_samples_wtheta=15,
+    #     n_samples_wphi=15,
+    #     reparam_latent=True,
+    #     n_epochs=100,
+    #     lr=3e-4,
+    # ),
+
     dict(
         loss_gen="ELBO",
         loss_wvar="CUBOB",
@@ -48,10 +71,27 @@ scenarios = [  # WAKE updates
         n_samples_wtheta=15,
         n_samples_wphi=15,
         reparam_latent=True,
-        n_epochs=100,
-        lr=3e-4,
+        z2_with_elbo=True,
+        n_epochs=75,
+        n_hidden=128,
+        n_latent=10,
+        lr=1e-3,
     ),
-    
+
+    dict(
+        loss_gen="ELBO",
+        loss_wvar="CUBOB",
+        n_samples_train=None,
+        n_samples_wtheta=15,
+        n_samples_wphi=15,
+        reparam_latent=True,
+        z2_with_elbo=False,
+        n_epochs=75,
+        n_hidden=128,
+        n_latent=10,
+        lr=1e-3,
+    ),
+
     dict(
         loss_gen="ELBO",
         loss_wvar="REVKL",
@@ -59,8 +99,25 @@ scenarios = [  # WAKE updates
         n_samples_wtheta=15,
         n_samples_wphi=15,
         reparam_latent=False,
-        n_epochs=100,
-        lr=3e-4,
+        z2_with_elbo=False,
+        n_epochs=75,
+        n_hidden=128,
+        n_latent=10,
+        lr=1e-3,
+    ),
+
+    dict(
+        loss_gen="ELBO",
+        loss_wvar="REVKL",
+        n_samples_train=None,
+        n_samples_wtheta=15,
+        n_samples_wphi=15,
+        reparam_latent=False,
+        z2_with_elbo=True,
+        n_epochs=75,
+        n_hidden=128,
+        n_latent=10,
+        lr=1e-3,
     ),
 
     dict(
@@ -70,144 +127,13 @@ scenarios = [  # WAKE updates
         n_samples_wtheta=None,
         n_samples_wphi=None,
         reparam_latent=True,
-        n_epochs=100,
-        lr=3e-4,
+        z2_with_elbo=False,
+        n_epochs=75,
+        n_hidden=128,
+        n_latent=10,
+        lr=1e-3,
     ),
 
-    # REGULAR
-    # dict(
-    #     loss_gen="ELBO",
-    #     loss_wvar="CUBOB",
-    #     n_samples_train=None,
-    #     n_samples_wtheta=15,
-    #     n_samples_wphi=15,
-    #     reparam_latent=True,
-    #     n_epochs=25,
-    #     lr=3e-4,
-    # ),
-    
-    # dict(
-    #     loss_gen="ELBO",
-    #     loss_wvar="REVKL",
-    #     n_samples_train=None,
-    #     n_samples_wtheta=15,
-    #     n_samples_wphi=15,
-    #     reparam_latent=False,
-    #     n_epochs=25,
-    #     lr=3e-4,
-    # ),
-
-    # dict(
-    #     loss_gen="ELBO",
-    #     loss_wvar="ELBO",
-    #     n_samples_train=None,
-    #     n_samples_wtheta=15,
-    #     n_samples_wphi=15,
-    #     reparam_latent=True,
-    #     n_epochs=25,
-    #     lr=3e-4,
-    # ),
-    # dict(
-    #     loss_gen="ELBO",
-    #     loss_wvar="ELBO+CUBO",
-    #     n_samples_train=None,
-    #     n_samples_wtheta=1,
-    #     n_samples_wphi=15,
-    #     reparam_latent=True,
-    #     n_epochs=N_EPOCHS,
-    #     lr=LR,
-    # ),
-
-    
-
-    # dict(
-    #     loss_gen="ELBO",
-    #     loss_wvar="ELBO+CUBO",
-    #     n_samples_train=None,
-    #     n_samples_wtheta=15,
-    #     n_samples_wphi=15,
-    #     reparam_latent=True,
-    #     n_epochs=N_EPOCHS,
-    #     lr=LR,
-    # ),
-
-    # dict(
-    #     loss_gen="ELBO",
-    #     loss_wvar="CUBO",
-    #     n_samples_train=None,
-    #     n_samples_wtheta=1,
-    #     n_samples_wphi=15,
-    #     reparam_latent=True,
-    #     n_epochs=N_EPOCHS,
-    #     lr=LR,
-    # ),
-
-    # dict(
-    #     loss_gen="ELBO",
-    #     loss_wvar="CUBO",
-    #     n_samples_train=None,
-    #     n_samples_wtheta=1,
-    #     n_samples_wphi=15,
-    #     reparam_latent=True,
-    #     n_epochs=50,
-    #     lr=LR,
-    # ),
-
-    # dict(
-    #     loss_gen="ELBO",
-    #     loss_wvar="CUBO",
-    #     n_samples_train=None,
-    #     n_samples_wtheta=15,
-    #     n_samples_wphi=15,
-    #     reparam_latent=True,
-    #     n_epochs=N_EPOCHS,
-    #     lr=LR,
-    # ),
-    
-
-    # dict(
-    #     loss_gen="ELBO",
-    #     loss_wvar="REVKL",
-    #     n_samples_train=None,
-    #     n_samples_wtheta=1,
-    #     n_samples_wphi=15,
-    #     reparam_latent=False,
-    #     n_epochs=N_EPOCHS,
-    #     lr=LR,
-    # ),
-
-    # dict(
-    #     loss_gen="ELBO",
-    #     loss_wvar="REVKL",
-    #     n_samples_train=None,
-    #     n_samples_wtheta=1,
-    #     n_samples_wphi=15,
-    #     reparam_latent=False,
-    #     n_epochs=50,
-    #     lr=LR,
-    # ),
-    # dict(
-    #     loss_gen="ELBO",
-    #     loss_wvar="ELBO",
-    #     n_samples_train=1,
-    #     n_samples_wtheta=None,
-    #     n_samples_wphi=None,
-    #     reparam_latent=True,
-    #     n_epochs=N_EPOCHS,
-    #     lr=LR,
-    # ),
-
-    # dict(
-    #     loss_gen="ELBO",
-    #     loss_wvar="ELBO",
-    #     n_samples_train=1,
-    #     n_samples_wtheta=None,
-    #     n_samples_wphi=None,
-    #     reparam_latent=True,
-    #     n_epochs=50,
-    #     lr=LR,
-    # ),
-    # (None, "ELBO", "CUBO", None, None, 1, 15, True),
 ]
 
 df_li = []
@@ -251,11 +177,18 @@ for scenario in scenarios:
     n_samples_wphi = scenario["n_samples_wphi"]
     reparam_latent = scenario["reparam_latent"]
     n_epochs = scenario["n_epochs"]
+    n_latent = scenario["n_latent"]
+    n_hidden = scenario["n_hidden"]
     lr = scenario["lr"]
+    z2_with_elbo = scenario["z2_with_elbo"]
 
     iwelbo = []
     cubo = []
     khat = []
+    khat1e4 = []
+    ess = []
+    khat1e4_supervised = []
+    ess_supervised = []
     m_accuracy_arr = []
     m_ap_arr = []
     m_recall_arr = []
@@ -268,13 +201,13 @@ for scenario in scenarios:
         for st in scenario.values():
             mdl_name = mdl_name + str(st) + "_"
         mdl_name = str(mdl_name)
-        mdl_name = "models/mnist/{}.pt".format(mdl_name)
+        mdl_name = os.path.join(MDL_DIR, "{}.pt".format(mdl_name))
         print(mdl_name)
         mdl = SemiSupervisedVAE(
             n_input=n_input,
             n_labels=n_labels,
-            n_latent=50,
-            n_hidden=500,
+            n_latent=n_latent,
+            n_hidden=n_hidden,
             n_layers=1,
             do_batch_norm=True,
         )
@@ -303,6 +236,7 @@ for scenario in scenarios:
                     n_samples_phi=n_samples_wphi,
                     reparam_wphi=reparam_latent,
                     classification_ratio=CLASSIFICATION_RATIO,
+                    z2_with_elbo=z2_with_elbo,
                     update_mode="all",
                 )
             torch.save(mdl.state_dict(), mdl_name)
@@ -356,6 +290,7 @@ for scenario in scenarios:
             m_accuracy_arr.append(m_accuracy)
 
             # k_hat
+            ## Old version taking few samples
             log_ratios = train_res["log_ratios"].cpu().numpy()
             n_examples = log_ratios.shape[-1]
             log_ratios = log_ratios.reshape((-1, n_examples)).T
@@ -363,6 +298,64 @@ for scenario in scenarios:
             _, khat_vals = psislw(log_ratios)
             khat.append(khat_vals)
 
+            ## New version
+            log_ratios = []
+            qc_z = []
+            n_samples_total = 1e4
+            n_samples_per_pass = 25
+            n_iter = int(n_samples_total / n_samples_per_pass)
+
+            ### Unsupervised case
+            for _ in tqdm(range(n_iter)):
+                with torch.no_grad():
+                    out = mdl.inference(x_sample, n_samples=n_samples_per_pass)
+                log_ratio = (
+                    out["log_px_z"]
+                    + out["log_pz2"]
+                    + out["log_pc"]
+                    + out["log_pz1_z2"]
+                    - out["log_qz1_x"]
+                    - out["log_qc_z1"]
+                    - out["log_qz2_z1"]
+                ).cpu()
+                qc_z_here = out["log_qc_z1"].cpu().exp()
+
+                qc_z.append(qc_z_here)
+                log_ratios.append(log_ratio)
+            # Concatenation over samples
+            log_ratios = torch.cat(log_ratios, 1)
+            qc_z = torch.cat(qc_z, 1)
+            log_ratios = (log_ratios * qc_z).sum(0)  # Sum over labels
+
+            _, khats = psislw(log_ratios.T.clone())
+            wi = torch.softmax(log_ratios, 0)
+            ess_here = 1.0 / (wi**2).sum(0)
+
+            khat1e4.append(khats)
+            ess.append(ess_here.numpy())
+
+            ### Supervised case
+            log_ratios = []
+            for _ in tqdm(range(n_iter)):
+                with torch.no_grad():
+                    out = mdl.inference(x_sample, n_samples=n_samples_per_pass)
+                out = (
+                    out["log_px_z"]
+                    + out["log_pz2"]
+                    + out["log_pz1_z2"]
+                    - out["log_qz1_x"]
+                    - out["log_qz2_z1"]
+                )
+                log_ratios.append(out.cpu())
+            log_ratios = torch.cat(log_ratios, dim=1)
+            log_ratios = log_ratios[8].T  # Only take label of interest
+
+            _, khats = psislw(log_ratios.clone())
+            wi = torch.softmax(log_ratios, 0)
+            ess_here = 1.0 / (wi**2).sum(0)
+
+            khat1e4_supervised.append(khats)
+            ess_supervised.append(ess_here.numpy())
         except Exception as e:
             print(e)
             pass
@@ -375,13 +368,20 @@ for scenario in scenarios:
         "N_SAMPLES_WTHETA": n_samples_wtheta,
         "N_SAMPLES_WPHI": n_samples_wphi,
         "REPARAM_LATENT": reparam_latent,
+        "N_LATENT": n_latent,
+        "N_HIDDEN": n_hidden,
         "N_EPOCHS": n_epochs,
         "LR": lr,
+        "Z2_WITH_ELBO": z2_with_elbo,
         "IWELBO": (np.mean(iwelbo), np.std(iwelbo)),
         "IWELBO_SAMPLES": np.array(iwelbo),
         "CUBO": (np.mean(cubo), np.std(cubo)),
         "CUBO_SAMPLES": np.array(cubo),
         "KHAT": np.array(khat),
+        "KHAT1e4": np.array(khat1e4),
+        "ESS": np.array(ess),
+        "KHAT1e4_supervised": np.array(khat1e4_supervised),
+        "ESS_supervised": np.array(ess_supervised),
         "M_ACCURACY": np.array(m_accuracy_arr),
         "MEAN_AP": np.array(m_ap_arr),
         "MEAN_RECALL": np.array(m_recall_arr),
@@ -391,9 +391,7 @@ for scenario in scenarios:
     print(res)
     df_li.append(res)
     df = pd.DataFrame(df_li)
-    df.to_csv("simu_mnist_res_paper.csv", sep="\t")
-    df.to_pickle("simu_mnist_res_paper.pkl")
+    df.to_pickle(FILENAME)
 
 df = pd.DataFrame(df_li)
-df.to_csv("simu_mnist_res_paper.csv", sep="\t")
-df.to_pickle("simu_mnist_res_paper.pkl")
+df.to_pickle(FILENAME)
