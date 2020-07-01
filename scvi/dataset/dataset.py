88f5031f6ac2d1c7ee0fda95a5724696ca71335b
galen xing <gx2113@columbia.edu>
2020-07-01 19:31:47 -0400

    remove pdb

--- scvi/dataset/dataset.py
+++ scvi/dataset/dataset.py
@@ -1871,7 +1871,6 @@ class DownloadableDataset(GeneExpressionDataset, ABC):
 def _download(url: str, save_path: str, filename: str):
     """Writes data from url to file.
     """
-    pdb.set_trace()
     if os.path.exists(os.path.join(save_path, filename)):
         logger.info("File %s already downloaded" % (os.path.join(save_path, filename)))
         return
