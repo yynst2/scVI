f6b0acacf06ca3d0d6da27f3f53bfc747b2d6b64
adamgayoso <adamgayoso@users.noreply.github.com>
2020-07-03 18:14:58 -0700

    update loom datasets

--- scvi/dataset/__init__.py
+++ scvi/dataset/__init__.py
@@ -4,6 +4,7 @@ from ._preprocessing import highly_variable_genes_seurat_v3
 from .cite_seq import pbmcs_10x_cite_seq
 from ._datasets import synthetic_iid
 from .cortex import cortex
+from .loom import retina, prefrontalcortex_starmap, frontalcortex_dropseq
 
 
 from .dataset import GeneExpressionDataset
