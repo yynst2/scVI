9ab8fc296e0e8f9cbd1345d893edea297afa6bc2
Adam Gayoso <adamgayoso@users.noreply.github.com>
2020-07-08 12:05:55 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -141,13 +141,19 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+
+html_theme_options = {
+    "repository_url": "https://github.com/YosefLab/scVI",
+    "use_repository_button": True,
+}
+
+
 html_context = dict(
     display_github=True,  # Integrate GitHub
     github_user="YosefLab",  # Username
