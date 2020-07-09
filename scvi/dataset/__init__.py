e39e29f8ec52be5d13e859f818eaae59285cb36d
adamgayoso <adamgayoso@users.noreply.github.com>
2020-07-09 14:45:47 -0700

    update dataset10x

--- scvi/dataset/__init__.py
+++ scvi/dataset/__init__.py
@@ -2,6 +2,7 @@ from ._anndata import setup_anndata
 from ._preprocessing import highly_variable_genes_seurat_v3
 
 from .cite_seq import pbmcs_10x_cite_seq
+from .dataset10X import dataset10X
 from ._datasets import synthetic_iid
 from .cortex import cortex
 from .seqfish import seqfish
